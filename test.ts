7285a84604936790c0da4ec5c8c9233e5e44fa0f
axetroy <axetroy.dev@gmail.com>
2020-01-11 10:56:00 +0800

    upgrade deno v0.29.0

--- test.ts
+++ test.ts
@@ -1,5 +1,5 @@
-import { assertEquals } from "https://deno.land/std@v0.27.0/testing/asserts.ts";
-import { runIfMain, test } from "https://deno.land/std@v0.27.0/testing/mod.ts";
+import { assertEquals } from "https://deno.land/std@v0.29.0/testing/asserts.ts";
+import { runIfMain, test } from "https://deno.land/std@v0.29.0/testing/mod.ts";
 import { parse } from "./mod.ts";
 
 test(async function testParse() {
